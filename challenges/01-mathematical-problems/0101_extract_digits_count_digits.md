76ec02aecb2137d5fd4c2805988e196e65bffbf4
Sefa Cihangir <sefa.cihangir@iottech.com.tr>
2022-08-21 19:50:35 +0300

    first commit

---
+++ challenges/01-mathematical-problems/0101_extract_digits_count_digits.md
@@ -0,0 +1,14 @@
+# Extraction of digits
+Girilen sayıyın rakamlarını tersten yazan metod yazınız.
+
+```
+void extractDigits(final int startValue)
+```
+
+
+# countDigits
+Girilen sayının kaç adet rakamı olduğunu dönen metod yazınız.
+
+```
+int countDigits(final int number)
+```
---
+++ src/main/java/com/phoenix/_01_mathematical_problems/_01/App0101.java
@@ -0,0 +1,24 @@
+package com.phoenix._01_mathematical_problems._01;
+
+public class App0101 {
+
+  public static void main(String[] args) {
+
+    final int number = 123;
+
+    extractDigits(number);
+
+    final int numberOfDigits = countDigits(number);
+    System.out.printf("%s has %s digits%n", number, numberOfDigits);
+  }
+
+  public static void extractDigits(final int startValue) {
+
+  }
+
+  public static int countDigits(final int number){
+
+    return 0;
+  }
+
+}
