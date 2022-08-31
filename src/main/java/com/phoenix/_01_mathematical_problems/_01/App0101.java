a39269cbc99317baa32322f977ba153e43e5b47f
Zafer Akgül <zafer.akgul@iottech.com.tr>
2022-08-31 22:55:09 +0300

    the problem is solved by CobraZafer problem 1

--- src/main/java/com/phoenix/_01_mathematical_problems/_01/App0101.java
+++ src/main/java/com/phoenix/_01_mathematical_problems/_01/App0101.java
@@ -4,31 +4,34 @@ public class App0101 {
 
   public static void main(String[] args) {
 
-    final int number = 6534627;
+    final int number = -6781;
+
 
     extractDigits(number);
 
     final int numberOfDigits = countDigits(number);
     System.out.printf("%s has %s digits%n", number, numberOfDigits);
+
   }
 
-  public static void extractDigits(final int startValue) {
-
+  public static void extractDigits(final int val) {
+    int startValue = Math.abs(val);
     if(startValue / 10 > 0) {
       extractDigits(startValue / 10);
     }
     System.out.printf("%s ", startValue % 10);
-
   }
 
-  public static int countDigits(final int number){
+  public static int countDigits(final int val){
+    int number = Math.abs(val);
+    int count = 0;
+    int divider = number;
+    while (divider > 0 ){
+      divider =  divider / 10;
+      count++;
+    }
+    return count;
+  }
 
-    if (number > 0){
-
-      return String.valueOf(number).length();
-    }else {
-
-    return 0;
-  }}
 
 }
