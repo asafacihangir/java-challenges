003d750ac4bcfffd0191ebf6cdfb5213e1ae629a
Zafer Akgül <zafer.akgul@iottech.com.tr>
2022-09-28 14:53:38 +0300

    proper divisors and prime numbers problems are solved by Cobra_Zafer

--- src/main/java/com/phoenix/_01_mathematical_problems/_01/App0102.java
+++ src/main/java/com/phoenix/_01_mathematical_problems/_01/App0102.java
@@ -1,16 +1,18 @@
 package com.phoenix._01_mathematical_problems._01;
 
-import java.util.Collections;
-import java.util.List;
-
 public class App0102 {
 
   public static void main(String[] args) {
 
+    findProperDivisors(1234);
   }
 
+  public static void findProperDivisors(final int value) {
 
-  public static List<Integer> findProperDivisors(final int value){
-    return Collections.emptyList();
+    for (int i = 1; i <= value; i++) {
+      if (value % i == 0) {
+        System.out.print(i + " - ");
+      }
+    }
   }
 }
--- src/main/java/com/phoenix/_01_mathematical_problems/_01/App0103.java
+++ src/main/java/com/phoenix/_01_mathematical_problems/_01/App0103.java
@@ -3,9 +3,24 @@ package com.phoenix._01_mathematical_problems._01;
 public class App0103 {
 
   public static void main(String[] args) {
-
+    isPrime(42);
   }
 
+  public static boolean isPrime(int value) {
+    boolean flag = false;
+    for (int i = 2; i <= value / 2; ++i) {
+      if (value % i == 0) {
+        flag = true;
+        break;
+      }
+    }
 
-
+    if (!flag) {
+      System.out.println(value + " is a prime number.");
+    }
+    else {
+      System.out.println(value + " is not a prime number.");
+    }
+    return flag;
+  }
 }
